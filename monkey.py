5b2bf1ae73c721c5c23e5c28efad49f0a1c41120
stu888 <splatoon8888@gmail.com>
2016-10-12 15:09:13 +0000

    First commit

---
+++ monkey.py
@@ -0,0 +1,11 @@
+import os, sys, traceback
+
+## comments
+# comments
+
+a = 1
+
+while a < 10:
+    a+= 1
+    print a
+
---
+++ raw_input.py
@@ -0,0 +1,5 @@
+print "how old are you?"
+age = raw_input()
+print "huh"
+height = raw_input()
+print "so youre %r old and %r high" % (age, height)
